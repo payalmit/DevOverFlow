6ab33f7a0eac37eb588b1650e7582e70f7d0c520
payalmit <payalmittal651@gmail.com>
2024-03-16 15:09:53 +0530

    Feat: Adding DB schema for question

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,9 +1,25 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   try {
     connectToDatabase();
+    const { title, content, tags, author, path } = params;
+    // create a question
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate({}, {}, {});
+    }
   } catch (err) {}
 }
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,16 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-let isConnected : boolean = false;
+let isConnected: boolean = false;
 
 export const connectToDatabase = async () => {
-    mongoose.set('strictQuery', true);  
-    
-    if(!process.env.MONGODB_URL){
-        return console.log("MISSING MONGODB_URL");
-    }
+  mongoose.set("strictQuery", true);
 
-    if(isConnected){
-          return 
-            }
-    
-}
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
+  }
+
+  if (isConnected) {
+    return console.log("MongoDB is already connected");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "devflow",
+    });
+    isConnected = true;
+
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB connection Failed", error);
+  }
+};
